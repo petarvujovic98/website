cf9b17190057acdc5151caa5738efecb16231bf7
Petar Vujovic <petarvujovic98@gmail.com>
2023-07-10 13:40:04 +0200

    feat: Add basic content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import image from "@astrojs/image";
+
+// https://astro.build/config
+
+// https://astro.build/config
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), image()]
 });
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Petar Vujovic";
+export const SITE_DESCRIPTION =
+  "I'm a software engineer with a need for building and learning";
