18a0f154ba711bdfe93e644819763383746ee2f4
Petar Vujović <petarvujovic98@gmail.com>
2023-11-13 20:23:42 +0100

    feat: Update design
    Update colors
    Update favicon
    Update copy
    Update links and screenshots
    Migrate to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
-import image from "@astrojs/image";
-
-// https://astro.build/config
-
-// https://astro.build/config
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), svelte(), image()]
+  integrations: [mdx(), sitemap(), tailwind(), svelte()]
 });
