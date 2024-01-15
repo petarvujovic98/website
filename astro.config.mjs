aa6b10ec757ae5f2eea856a325fb435ef3fb894a
Petar Vujovic <petarvujovic98@gmail.com>
2024-01-15 11:55:04 +0100

    feat: Update website work and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), mdx(), sitemap()],
 });
